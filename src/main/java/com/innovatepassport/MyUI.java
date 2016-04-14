adc670bb7f094d2a51758c25d6cdc35c05b3c4c6
Adam Trojnar <trojnaradam@gmail.com>
2016-04-15 00:02:42 +0200

    added VaadinDesigner theme fix

---
+++ src/main/java/com/innovatepassport/MyUI.java
@@ -0,0 +1,27 @@
+package com.innovatepassport;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.UI;
+
+import javax.servlet.annotation.WebServlet;
+
+/**
+ *
+ */
+@Theme("mytheme")
+public class MyUI extends UI {
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        setContent(new MainLayout());
+
+    }
+
+    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = MainGUI.class, productionMode = false)
+    public static class MyUIServlet extends VaadinServlet {
+    }
+}
---
+++ src/main/java/com/innovatepassport/PluginsView.java
@@ -0,0 +1,16 @@
+package com.innovatepassport;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+
+public class PluginsView extends PluginsViewDesign implements View {
+
+    public static final String VIEW_NAME = "plugins";
+
+    @Override
+    public void enter(ViewChangeEvent event) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
