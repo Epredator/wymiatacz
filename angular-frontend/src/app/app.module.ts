dd823ade721aaa945909913b5eb9ef994c66e386
epredator <trojnaradam@gmail.com>
2019-01-31 23:49:22 +0100

    added Products component

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductsComponents } from './products.components';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponents
   ],
   imports: [
     BrowserModule
